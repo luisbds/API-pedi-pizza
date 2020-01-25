415742131f10a4eb4fce51fc13f2756281d49a17
luisbds <duduluis559@hotmail.com>
2020-01-25 15:36:49 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const PizzaController = require('./controllers/Pizza-controller')
 const routes = express.Router()
+
+routes.post('/pizza', PizzaController.store) 
 
 module.exports = routes
 
