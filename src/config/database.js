904a981b090df3e26dcc0edf988ea614825080dd
luisbds <duduluis559@hotmail.com>
2020-01-25 14:52:01 -0300

    up

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'asd321',
+    database: 'api-pizza',
+    define: {
+        timestamps: true,
+        underscored: true 
+    }
+}
