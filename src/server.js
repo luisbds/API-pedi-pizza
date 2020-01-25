b5dab0050192033ad59453e8128d61227b7fed33
luisbds <duduluis559@hotmail.com>
2020-01-25 14:22:01 -0300

    up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('Olá')
+})
+
 app.listen(3001, () => {
     console.log('Servidor rodando na porta 3001')
 })
