006f0d0ac1404c785524c627cb31385f3cf17c88
luisbds <duduluis559@hotmail.com>
2020-01-25 23:19:06 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,11 @@ const PedidoController = require('./controllers/Pedido-controller')
 const ItemPedidoController = require('./controllers/ItemPedido-controller')
 const routes = express.Router()
 
-routes.post('/itempedido', ItemPedidoController.store) 
+routes.get('/', (req, res) => {
+    return res.json({hello: "Olá"})
+}) 
+
+routes.post('/itemPedido', ItemPedidoController.store) 
 routes.post('/pedido', PedidoController.store) 
 routes.post('/pizza', PizzaController.store) 
 
--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ const app = express()
 app.use(express.json())
 
 app.use(routes) 
-
+/*
 app.get('/', (req, res) => {
     res.send('Olá')
 }) 
-
+*/
 app.listen(3001, () => {
     console.log('Servidor rodando na porta 3001')
 })
