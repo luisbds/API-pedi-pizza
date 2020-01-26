4093869bcd391e357931a76aa49cb062e3aa969b
luisbds <duduluis559@hotmail.com>
2020-01-26 20:59:18 -0300

    up

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const PizzaController = require('./controllers/Pizza-controller')
 const PedidoController = require('./controllers/Pedido-controller')
-const ItemPedidoController = require('./controllers/ItemPedido-controller')
-const PedidoPizzaController = require('./controller/PedidoPizza-controller')
+const ItemPedidoController = require('./controllers/ItemPedido-controller') 
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
     return res.json({hello: "Olá"})
 }) 
 
-routes.post('/pedidopizza', PedidoPizzaController.store)
 routes.post('/itemPedido', ItemPedidoController.store) 
 routes.post('/pedido', PedidoController.store) 
 routes.post('/pizza', PizzaController.store) 
