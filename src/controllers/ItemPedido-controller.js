c087bb720ac65011cc2562bc0fd5286e86e6b4c9
luisbds <duduluis559@hotmail.com>
2020-01-26 00:35:09 -0300

    up

--- src/controllers/ItemPedido-controller.js
+++ src/controllers/ItemPedido-controller.js
@@ -1,19 +1,25 @@
 const ItemPedido = require('../model/itemPedido')
+const Pedido = require('../model/pedido') 
 
 module.exports = {
     async store (req, res) {
         const {pedido_id} = req.params
         const {pizza, quantidade} = req.body
 
-        const find = await ItemPedido.findByPk(pedido_id)
+        const pedido = await Pedido.findByPk(pedido_id)
         
-        if(!find){ 
+        if(!pedido){ 
             return res.status(400).json({
-                erro:"Usuario não existe"})
+                erro:"Item não existe"})
         }
-
+        /*
         const itempedido = await ItemPedido.create({pizza, quantidade}) 
+        */
+       const [itempedido, created] = await ItemPedido.findOrCreate({
+           where: {pizza, quantidade} 
+       })
 
+       await pedido.addItemPedido(itempedido) 
         return res.json(itempedido) 
     }
 }
--- src/controllers/Pedido-controller.js
+++ src/controllers/Pedido-controller.js
@@ -1,17 +1,25 @@
 const Pedido = require('../model/pedido')
+const Pizza = require('../model/pizza') 
 
 module.exports = {
     async store (req, res) {
         const {pizza_id} = req.params
         const {item} = req.body
 
-        const find = await Pedido.findByPk(pizza_id)
+        const pizza = await Pizza.findByPk(pizza_id)
 
-        if(!find){
+        if(!pizza){ 
             return res.status(400).json({
-                erro:"Usuario não existe"})
+                erro:"Pedido não existe"})
         }
+        /*
         const pedido = await Pedido.create({item})
+        */
+        const [pedido, created] = await Pedido.findOrCreate({
+            where : {item} 
+        }) 
+
+        await pizza.addPedido(pedido) 
 
         return res.json(pedido) 
     }
