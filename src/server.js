88834ccf2ecfe4186b5767f874d752e91e1ac4b7
luisbds <duduluis559@hotmail.com>
2020-01-26 20:46:19 -0300

    up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
-
+const { pedidoPizza, pedidoPizza2} = require('./model') 
 require('./database')
 
 const app = express()
@@ -8,6 +8,26 @@ const app = express()
 app.use(express.json())
 
 app.use(routes) 
+
+app.post('/pedidoPizza', async(req, res) => {
+    try {
+        const PedidoPizza = await pedidoPizza.create(req.body)
+
+        return res.status(200).json(PedidoPizza)
+    } catch (err) {
+        return res.status(500).json({ err }) 
+    }
+})
+
+app.post('/pedidoPizza2', async(req, res) => {
+    try {
+        const PedidoPizza2 = await pedidoPizza2.create(req.body)
+
+        return res.status(200).json(PedidoPizza2)
+    } catch (err) {
+        return res.status(500).json({ err })
+    }
+})
 /*
 app.get('/', (req, res) => {
     res.send('Olá')
